29c54a7ab0a788380b9858c045f9db637cf99d4e
mas117 <sitnikovamasha2004@gmail.com>
2023-07-06 19:02:51 +0700

    2part_11: The sum of integers from number a to number b
    This program finds the sum of all integers from a to b inclusive (the values of a and b are entered from the keyboard).

---
+++ 2part_11.py
@@ -0,0 +1,6 @@
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+summ = 0
+for i in range(a, b+1):
+    summ += i
+print("Сумма всех целых чисел от "+str(a)+" до "+str(b)+" равна "+str(summ))
