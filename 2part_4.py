44e6524645ed2b2a0ef16ae2045af655182465ee
mas117 <sitnikovamasha2004@gmail.com>
2023-07-06 18:27:14 +0700

    2part_4: Solving the quadratic equation
    The values of the coefficients a, b, c are entered from the keyboard. The program checks the value of the discriminant: if it is less than zero, there are no roots, if it is zero, then the root is 1, if it is greater than zero, there are two roots. To do this, a construction of the form if elif else is used.

---
+++ 2part_4.py
@@ -0,0 +1,14 @@
+import math
+input("Введите значения коэффициентов квадратного уравнения:")
+a = int(input("a="))
+b = int(input("b="))
+c = int(input("c="))
+D = b**2-4*a*c
+print("Ответ:")
+if D < 0:
+    print("Корней нет")
+elif D == 0:
+    print("Корень один: ", -b//(2*a))
+else:
+    print("Первый корень: ", (-b+math.sqrt(D))//(2*a))
+    print("Второй корень: ", (-b-math.sqrt(D))//(2*a))
