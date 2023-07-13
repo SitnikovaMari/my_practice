300fdb852f1c597c91987d0ea807a74829ec0716
mas117 <sitnikovamasha2004@gmail.com>
2023-07-13 21:35:38 +0700

    Pt_4_1: Maximum number
    This is a program that finds the largest number obtained by permuting the digits of a given number.

---
+++ Pt_4_1.py
@@ -0,0 +1,8 @@
+n = int(input("Введите число: "))
+num = list(str(abs(n)))
+if n >= 0:
+    num.sort(reverse=True)
+    print("Максимальное число:", "".join(num))
+else:
+    num.sort()
+    print("Максимальное число:", int("".join(num))*(-1))
