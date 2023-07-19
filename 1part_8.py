ae84d08fff5f7f3f3bbc77eecd1ebd92019704c9
mas117 <sitnikovamasha2004@gmail.com>
2023-07-19 21:18:56 +0700

    Line break removed.

--- 1part_8.py
+++ 1part_8.py
@@ -4,6 +4,6 @@ if age % 10 == 1:
     print("Привет, " + name + "! Тебе уже " + str(age) + " год.")
 if age % 10 == 2 or age % 10 == 3 or age % 10 == 4:
     print("Привет, " + name + "! Тебе уже " + str(age) + " года.")
-if (age % 10 == 0 or age % 10 == 5 or age % 10 == 6 or
-        age % 10 == 7 or age % 10 == 8 or age % 10 == 9):
+if age % 10 == 0 or age % 10 == 5 or age % 10 == 6 or \
+        age % 10 == 7 or age % 10 == 8 or age % 10 == 9:
     print("Привет, " + name + "! Тебе уже " + str(age) + " лет.")
