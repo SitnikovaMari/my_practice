12f6a1a7bacf62c640329ec7636997d69be7509d
mas117 <sitnikovamasha2004@gmail.com>
2023-07-20 14:06:16 +0700

    Added spaces around arithmetic operators and a new line at the end of the file.

--- 2part_4.py
+++ 2part_4.py
@@ -3,12 +3,12 @@ input("Введите значения коэффициентов квадрат
 a = int(input("a="))
 b = int(input("b="))
 c = int(input("c="))
-D = b**2-4*a*c
+D = b ^ 2 - 4 * a * c
 print("Ответ:")
 if D < 0:
     print("Корней нет")
 elif D == 0:
-    print("Корень один: ", -b//(2*a))
+    print("Корень один: ", -b // (2 * a))
 else:
-    print("Первый корень: ", (-b+math.sqrt(D))//(2*a))
-    print("Второй корень: ", (-b-math.sqrt(D))//(2*a))
+    print("Первый корень: ", (-b + math.sqrt(D)) // (2 * a))
+    print("Второй корень: ", (-b - math.sqrt(D)) // (2 * a))
