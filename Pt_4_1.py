fd31cb45fea12e4c46b6241090efa85902d3e44e
mas117 <sitnikovamasha2004@gmail.com>
2023-07-20 14:44:47 +0700

    Added spaces around arithmetic operators and a new line at the end of the file.

--- Pt_4_1.py
+++ Pt_4_1.py
@@ -5,4 +5,4 @@ if n >= 0:
     print("Максимальное число:", "".join(num))
 else:
     num.sort()
-    print("Максимальное число:", int("".join(num))*(-1))
+    print("Максимальное число:", int("".join(num)) * (-1))
--- Pt_4_3.py
+++ Pt_4_3.py
@@ -1,7 +1,7 @@
 n1 = int(input("Введите превое число диапазона: "))
 n2 = int(input("Введите второе число диапазона: "))
 print("Простые числа заданного диапазона:")
-for i in range(n1, n2+1):
+for i in range(n1, n2 + 1):
     for j in range(2, i):
         if i % j == 0:
             break
