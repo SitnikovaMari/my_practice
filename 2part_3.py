68f77765617b100ed4fe30b48b7e32bdb6978e3b
mas117 <sitnikovamasha2004@gmail.com>
2023-07-20 14:02:35 +0700

    Changed the variable name, added spaces around arithmetic operators and a new line at the end of the file.

--- 2part_3.py
+++ 2part_3.py
@@ -1,11 +1,11 @@
 number = int(input("Введите число для проверки: "))
 n = number
-l= len(str(number))
+length = len(str(number))
 summ = 0
 while number != 0:
     c = number % 10
-    summ = summ+(c**l)
-    number = number//10
+    summ = summ + (c ^ length)
+    number = number // 10
 if n == summ:
     print("Число", n, "является числом Армстронга")
 else:
