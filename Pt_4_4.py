11b2df0f01ea94fbb81c2615bd8587f7535859b6
mas117 <sitnikovamasha2004@gmail.com>
2023-07-20 14:47:50 +0700

    Added spaces around arithmetic operators, 2 empty lines after the function definition and a new line at the end of the file.

--- Pt_4_4.py
+++ Pt_4_4.py
@@ -8,11 +8,13 @@ def find_combinations(lst, num, combinations=[]):
         n = lst[i]
         remain = lst[i + 1:]
         find_combinations(remain, num, combinations + [n])
+
+
 number = int(input("Введите число: "))
 N = int(input("Введите длину списка чисел: "))
 lst_numbers = []
 print("Заполните список числами:")
 for i in range(N):
     lst_numbers.append(int(input()))
-print("Найденные комбинации чисел, сумма которых равна "+str(number)+":")
+print("Найденные комбинации чисел, сумма которых равна " + str(number) + ":")
 print(find_combinations(lst_numbers, number))
