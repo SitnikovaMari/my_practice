689ef6389689a954dcc686b14af50c83ba000e97
mas117 <sitnikovamasha2004@gmail.com>
2023-07-20 14:34:38 +0700

    Changed the variable name, added spaces around arithmetic operators and a new line at the end of the file.

--- Pt_3_1.py
+++ Pt_3_1.py
@@ -4,5 +4,5 @@ n = int(input("Введите длину списка чисел: "))
 for i in range(n):
     lst.append(random.randint(2, 50))
 print(lst)
-sred = lambda l, n: sum(l)/n
+sred = lambda lst, n: sum(lst) / n
 print("Среднее значение списка чисел: ", sred(lst, n))
